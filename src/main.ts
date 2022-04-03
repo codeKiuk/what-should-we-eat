1dc0b41fbc5949eaa343e3dde2774d16a86dbd64
양기욱 <kiuk@yang-giugs-MacBook-Air.local>
2022-04-03 16:28:10 +0900

    firebase config env 설정

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { getAuth, Auth } from "@firebase/auth";
 import { getFirestore, Firestore } from "@firebase/firestore";
 import firebaseConfig from "../firebaseConfig";
 
+console.log(process.env);
 export const app: FirebaseApp = initializeApp(firebaseConfig);
 export const db: Firestore = getFirestore(app);
 export const firebaseAuth: Auth = getAuth(app);
